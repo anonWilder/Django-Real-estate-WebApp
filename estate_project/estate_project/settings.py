c11408417177c6c0d63531f43e4a1077c3057b0d
anonWilder <austineokafor.e@gmail.com>
2023-06-28 14:37:00 +0100

    ADDED AN ARGUMENT OF * TO THE ALLOWED HOSTS

--- estate_project/estate_project/settings.py
+++ estate_project/estate_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-shp7+mj-nwil(h_a4=wvcc_koge@_p*m80l)&i5fpsd4-3rv)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
