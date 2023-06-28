2333e648f0ba3411167cf7f6b2035ff173a67a66
anonWilder <austineokafor.e@gmail.com>
2023-06-28 15:24:29 +0100

    staticfiles updating

--- estate_project/estate_project/settings.py
+++ estate_project/estate_project/settings.py
@@ -14,7 +14,14 @@ SECRET_KEY = 'django-insecure-shp7+mj-nwil(h_a4=wvcc_koge@_p*m80l)&i5fpsd4-3rv)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    'zaranrealestate.up.railway.app',
+    '*'
+    # Add any other allowed hosts here
+]
+
 
 
 # Application definition
